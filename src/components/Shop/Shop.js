4d26b9d270b0f41f1589b15ce87569fb8cf013fc
webdev-chinmoy-biswas <chinmoy709@gmail.com>
2022-09-28 16:38:30 +0600

    added load more

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,33 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import Product from '../Product/Product';
-import "./Shop.css"
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([])
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    useEffect(()=> {
-        fetch("products.json")
-        .then(res => res.json())
-        .then(data =>setProducts(data))
-    },[])
+  const handleAddToCart = (product) => {
+    console.log(product);
+  };
 
-    const handleAddToCart = (product) => {
-        console.log(product);
-      };
+  const [productCount, setProductCount] = useState(6)
 
-    return (
-        <div className='container shop-container'>
-            <div className="products-container">
-                {
-                    products.map( product => <Product key={product.id} product={product} handle={handleAddToCart}></Product>)
-                }
-            </div>
-            <div className="cart-container border">
-                This is cart
-            </div>
-        </div>
-    );
+  const handleLoadMore = () => {
+    setProductCount(productCount + 3);
+  };
+
+  return (
+    <div className="container shop-container">
+      <div>
+      <div className="products-container">
+        {products.slice(0, productCount).map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handle={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="load-more">
+        <button onClick={() => handleLoadMore()} className="btn load-more-btn">
+          Load More
+        </button>
+      </div>
+      </div>
+      <div className="cart-container border">This is cart</div>
+    </div>
+  );
 };
 
 export default Shop;
